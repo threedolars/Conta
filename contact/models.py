0813a23acac101104bb28818092b0b59f73401c0
arthursantanna <memo04sc@gmail.com>
2023-08-01 13:38:20 -0300

    Building Models

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# Create your models here:
+# id (primary key - automático)
+# first_name (string), last_name (string), phone (string)
+# email (email), created_date (date), description (text)
+
+# Later
+# category (foreign key), show (boolean), owner (foreign key)
+# picture (imagem)
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)  # Optional
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'

a497a89ed8d7fa20708e426355c36575f7b774de
arthursantanna <memo04sc@gmail.com>
2023-08-02 13:33:25 -0300

    Adding ForeignKey

--- contact/models.py
+++ contact/models.py
@@ -11,6 +11,13 @@ from django.utils import timezone
 # picture (imagem)
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)  # Optional
@@ -20,6 +27,9 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(Category,
+                                 on_delete=models.SET_NULL,
+                                 blank=True, null=True)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'

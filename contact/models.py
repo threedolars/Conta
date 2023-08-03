35e6981d3cc092f11b6ba5ea741bce21f50733e4
arthursantanna <memo04sc@gmail.com>
2023-08-03 08:53:06 -0300

    Adding owner user

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here:
 # id (primary key - automático)
@@ -12,6 +13,10 @@ from django.utils import timezone
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
@@ -30,6 +35,9 @@ class Contact(models.Model):
     category = models.ForeignKey(Category,
                                  on_delete=models.SET_NULL,
                                  blank=True, null=True)
+    owner = models.ForeignKey(User,
+                              on_delete=models.SET_NULL,
+                              blank=True, null=True)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'

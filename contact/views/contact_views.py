657aa5938cb34452c4ae2f844ef8cb20e88294a4
arthursantanna <memo04sc@gmail.com>
2023-08-08 08:55:05 -0300

    Building grown

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,13 +1,46 @@
-from django.shortcuts import render, get_object_or_404
+# flake8: noqa
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
+from django.db.models import Q
 # Create your views here.
 
 
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')[0:10]
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_object = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts
+        'page_object': page_object,
+        'site_title': 'Contacts - '
+    }
+
+    return render(request, 'contact/index.html', context)
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects.filter(show=True).filter(
+        Q(email__icontains=search_value) |
+        Q(phone__icontains=search_value) |
+        Q(first_name__icontains=search_value) |
+        Q(last_name__icontains=search_value)).order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_object = paginator.get_page(page_number)
+
+    context = {
+        'page_object': page_object,
+        'site_title': 'Search - ',
+        'search_value': search_value
     }
 
     return render(request, 'contact/index.html', context)
@@ -17,8 +50,20 @@ def contact(request, contact_id):
     single_contact = get_object_or_404(
         Contact.objects.filter(pk=contact_id), show=True)
 
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+
     context = {
-        'contact': single_contact
+        'contact': single_contact,
+        'site_title': site_title
     }
 
     return render(request, 'contact/contact.html', context)
+
+# CRUD
+
+
+def create(request):
+    context = {
+    }
+
+    return render(request, 'contact/_create.html', context)
